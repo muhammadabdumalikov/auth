b191dcc18f0cd61628dcc128a7017085eb5ad1db
MuhammadAbdumalik <burakali424@gmail.com>
2021-06-30 12:05:55 +0500

    Add File Structure

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path')
+const fs = require('fs')
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", req, (res) => {});
+app.listen(8000, () => console.log("Server ready"));
 
-app.listen(80, () => console.log("Server ready"));
+fs.readdir(path.join(__dirname, "routes"), (err, files) => {
+  files.forEach(file => {
+    const router = require(path.join(__dirname, "routes", file))
+    if(router.path && router.route) app.use(router.path, router.route)
+  })
+})
